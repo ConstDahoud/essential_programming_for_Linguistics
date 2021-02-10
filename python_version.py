be82926b4897a37c4b4154919adf29c4810efbc6
DaHun Shin <ekgnsdlek1@gmail.com>
2021-02-10 12:39:49 +0900

    Check the version of Python

---
+++ python_version.py
@@ -0,0 +1,3 @@
+import sys
+print(sys.version_info)    # sys.version_info(major=3, minor=9, micro=1, releaselevel='final', serial=0)
+print(sys.version)    # 3.9.1 (tags/v3.9.1:1e5d33e) [MSC v.1927 64 bit (AMD64)]
